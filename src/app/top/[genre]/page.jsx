82162f79fe47d52e73d33634891736543857271e
Dee-125 <kdkdenzel125@gmail.com>
2025-07-21 15:02:53 +0300

    Update Home component to fetch genre-specific movie data: Replaced static content with dynamic Results component, implemented error handling for API requests, and utilized environment variable for API key.

--- src/app/top/[genre]/page.jsx
+++ src/app/top/[genre]/page.jsx
@@ -1,5 +1,23 @@
-export default function TopPages() {
+import Results from "@/components/Results";
+
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+export default async function Home({ params }) {
+    const { genre } = await params;
+    const req = await fetch(
+        `https://api.themoviedb.org/3${
+            genre === 'rated'?  '/movie/top_rated' : '/trending/all/week'
+        }?api_key=${API_KEY}&language=en-US&page=1`
+    );
+    const data = await req.json();
+    if (!req.ok) {
+        throw new Error('Failed to Fetch data');
+    }
+    const results = data.results;
+
     return (
-        <div>TopPages</div>
+        <div>
+            <Results results = {results} />
+        </div>
     );
 }
