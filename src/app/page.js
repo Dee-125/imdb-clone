152bbe2781dd32042526b885f0545714ad30c1e9
Dee-125 <kdkdenzel125@gmail.com>
2025-07-21 13:01:42 +0300

    Enhance Home component and update Navbar styling: Integrated API data fetching in Home component and replaced welcome message with Results component; modified Navbar background color for improved visibility.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,21 @@
-export default function Home() {
+import Results from '@/components/Results';
+
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+export default async function Home() {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  };
+
+  const results = data.results;
+
   return (
-    <div className="p-4 text-2xl text-red-700">
-      Welcome to my clone app...
+    <div>
+      <Results results = {results} />
     </div>
   );
 }
